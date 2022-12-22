35eb9a5ea2b465dcccced9e3f036cbdd738a00b4
Benjamin Issa <benjamin.issa@protonmail.com>
2022-12-21 20:13:14 -0500

    setup networking for whats-yacking

--- environments/production/02-networking/main.tf
+++ environments/production/02-networking/main.tf
@@ -2,6 +2,6 @@ module "networking" {
   source = "../../../modules/networking"
 
   # VPC
-  vpc_name = "ams3-vpc-toot-community-01"
+  vpc_name = "sfo3-vpc-whatsyacking-us-01"
   vpc_cidr = "10.110.0.0/20"
 }
--- environments/production/02-networking/state.tf
+++ environments/production/02-networking/state.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    endpoint                    = "https://ams3.digitaloceanspaces.com"
+    endpoint                    = "https://sfo3.digitaloceanspaces.com"
     region                      = "us-east-1"
-    bucket                      = "state-toot-community"
+    bucket                      = "state-whatsyacking-us"
     key                         = "production/02-networking.tfstate"
   }
 }
